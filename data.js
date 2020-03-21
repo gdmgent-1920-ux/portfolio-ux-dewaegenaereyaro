279a49e7aaf0f87fd5e599ac0c924f6dd186cd98
dewaegenaereyaro <yarodewa@student.arteveldehs.be>
2020-03-21 19:54:48 +0100

    Opdracht 2 ingediend

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/579b1999-c10f-491e-4d91-7047642105a7-21aa/",
                 "target": "_blank"
             }
         ]
